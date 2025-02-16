40f81d7232462f608a20a3638231a91e10d9a643
Glennholhol <156083634+Glennholhol@users.noreply.github.com>
2025-02-16 09:45:04 +0000

    portfolio

--- my-laravel-app/database/migrations/0001_01_01_000001_create_cache_table.php
+++ my-laravel-app/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -11,16 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
-
-        Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->string('owner');
-            $table->integer('expiration');
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->timestamps();
         });
     }
 
@@ -29,7 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        Schema::dropIfExists('items');
     }
 };
